9edcc10f73c4fe7fa96a22a15f71012c72744fa0
Tharaka Athukorala <52407424+TharakaAthukorala@users.noreply.github.com>
2021-06-26 12:52:39 +0530

    Front End
    using React Js, Material UI

---
+++ Front End/src/pages/register/Register.jsx
@@ -0,0 +1,82 @@
+import { useRef } from "react";
+import "./register.css";
+import { useHistory } from "react-router";
+import axios from "axios"
+
+export default function Register() {
+
+  const username = useRef();
+  const email = useRef();
+  const password = useRef();
+  const passwordAgain = useRef();
+  const history = useHistory();
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    if(passwordAgain.current.value !== password.current.value) {
+      passwordAgain.current.setCustomValidity("Passwords don't match!");
+    } 
+    else {
+      const user = {
+        username: username.current.value,
+        email: email.current.value,
+        password: password.current.value,
+      };
+      try {
+        await axios.post("/auth/register", user);
+        history.push("/login");
+      } 
+      catch (err) {
+        console.log(err);
+      }
+    }
+  };
+
+  return (
+    <div className="login">
+      <div className="loginWrapper">
+        <div className="loginLeft">
+          <h3 className="loginLogo">UCSC Social</h3>
+          <span className="loginDesc">
+          Connect with friends and share What's in your mind? feel around you on <b>UCSC Social</b>
+          </span>
+        </div>
+        <div className="loginRight">
+          <form className="loginBox" onSubmit={handleClick}>
+            <input 
+              placeholder="Username" 
+              required 
+              ref={username} 
+              className="loginInput" 
+            />
+            <input 
+              placeholder="Email" 
+              required 
+              ref={email} 
+              className="loginInput" 
+              type="email"
+            />
+            <input 
+              placeholder="Password" 
+              required 
+              ref={password} 
+              className="loginInput" 
+              type="password"
+            />
+            <input 
+              placeholder="Password Again" 
+              required 
+              ref={passwordAgain} 
+              className="loginInput" 
+              type="password"
+            />
+            <button className="loginButton" type="submit">Sign Up</button>
+            <button className="loginRegisterButton">
+              Log into Account
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+}
